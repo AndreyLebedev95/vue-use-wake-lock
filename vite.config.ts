33dd10927708e7c8cb0518f1398cb722210b4a0a
AndreyLebedev95 <zzzrolfzzz@gmail.com>
2025-08-11 16:32:07 +0100

    feat: clean and setup build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+      outDir: 'dist',
+      insertTypesEntry: true,
+    }),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueUseWakeLock',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
